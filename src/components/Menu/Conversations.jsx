70c95e351cf1ad60f60ecf226bc8dda6421b83ea
Moin3 <moinislam667@gmail.com>
2024-03-23 22:56:04 +0600

    sidebar fix

--- src/components/Menu/Conversations.jsx
+++ src/components/Menu/Conversations.jsx
@@ -3,17 +3,20 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
+// import ListItemText from '@mui/material/ListItemText';
 import Avatar from '@mui/material/Avatar'
 import { userInfo } from '../../context/UserProvider';
 import { Link } from 'react-router-dom';
 import { postAPI } from '../../services/api';
 import { userAuth } from '../../context/AccountProvider';
+import { Box, Typography } from '@mui/material';
 
 
 const Conversations = ({users,index}) => {
     const {setPerson} =userInfo()
     const {user}=userAuth()
+
+    const msgText="The name of my country is Bangladesh"
 
 
     const getUser = async () => {
@@ -36,7 +39,15 @@ const Conversations = ({users,index}) => {
                         sx={{ width: 24, height: 24 }}
                     />
                 </ListItemIcon>
-                <ListItemText sx={{textTransform:'capitalize'}} primary={`${users?.first_name} ${users?.last_name}`} />
+                <Box sx={{display:'flex',flexDirection:'column'}}>
+                    {/* <ListItemText sx={{textTransform:'capitalize'}} primary={`${users?.first_name} ${users?.last_name}`} /> */}
+                    <h6 sx={{fontSize:'l',textTransform:'capitalize',fontWeight:'bold'}}>
+                         {`${users?.first_name} ${users?.last_name}`}
+                    </h6>
+                    <Typography sx={{mb:'0px',fontSize:'small',letterSpacing:'.5px',fontWeight:'semi-bold'}}>
+                        {msgText.slice(0, 15) + '...'}
+                    </Typography>
+                </Box>
                 </ListItemButton>
             </ListItem>
         </Link>
