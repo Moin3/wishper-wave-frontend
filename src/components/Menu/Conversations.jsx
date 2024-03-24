3a896c04e2d47b42eb31e5d539be5421e1f810e8
Moin3 <moinislam667@gmail.com>
2024-03-25 02:12:24 +0600

    apply active menu

--- src/components/Menu/Conversations.jsx
+++ src/components/Menu/Conversations.jsx
@@ -13,44 +13,52 @@ import { Box, Typography } from '@mui/material';
 const Conversations = ({ users }) => {
   const { setPerson } = userInfo();
   const { user } = userAuth();
+  const [selectedUser, setSelectedUser] = useState(null);
   const msgText = "The name of my country is Bangladesh";
 
-
   const getUser = async (userData) => {
-    const response=await postAPI('/conversation/add', { senderId: user?._id, receiverId: userData?._id });
+    const response = await postAPI('/conversation/add', { senderId: user?._id, receiverId: userData?._id });
     setPerson(userData);
-
+    setSelectedUser(userData); 
   };
 
   return (
     <List>
-        {users?.map((userData, index) => (
-            userData?.email !== user?.email && (
-            <ListItem key={index} disablePadding>
-                <Link to={'/'} style={{ textDecoration: 'none', color: 'black' }}>
-                <ListItemButton onClick={() => getUser(userData)}>
-                    <ListItemIcon>
-                    <Avatar
-                        alt="Remy Sharp"
-                        src={userData?.avatar}
-                        sx={{ width: 24, height: 24 }}
-                    />
-                    </ListItemIcon>
-                    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                    <Typography sx={{ textTransform: 'capitalize', fontWeight: 'bold' }}>
-                        {`${userData?.first_name} ${userData?.last_name}`}
-                    </Typography>
-                    <Typography sx={{ fontSize: 'small', letterSpacing: '.5px', fontWeight: 'semi-bold' }}>
-                        {msgText.slice(0, 15) + '...'}
-                    </Typography>
-                    </Box>
-                </ListItemButton>
-                </Link>
+      {users?.map((userData, index) => (
+        userData?.email !== user?.email && (
+          <Link key={index} to={'/'} style={{ textDecoration: 'none', color: 'black' }}>
+            <ListItem disablePadding>
+              <ListItemButton
+                onClick={() => getUser(userData)}
+                // selected={selectedUser?._id === userData._id} // 
+                sx={{
+                    backgroundColor: selectedUser?._id === userData._id ? '#c4e0f5' : 'transparent', 
+                    '&:hover': {
+                      backgroundColor: '#e0e0e0', // 
+                    },
+                  }}
+              >
+                <ListItemIcon>
+                  <Avatar
+                    alt="Remy Sharp"
+                    src={userData?.avatar}
+                    sx={{ width: 24, height: 24 }}
+                  />
+                </ListItemIcon>
+                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                  <Typography sx={{ textTransform: 'capitalize', fontWeight: 'bold' }}>
+                    {`${userData?.first_name} ${userData?.last_name}`}
+                  </Typography>
+                  <Typography sx={{ fontSize: 'small', letterSpacing: '.5px', fontWeight: 'semi-bold' }}>
+                    {msgText.slice(0, 15) + '...'}
+                  </Typography>
+                </Box>
+              </ListItemButton>
             </ListItem>
-            )
-        ))}
+          </Link>
+        )
+      ))}
     </List>
-
   );
 };
 
