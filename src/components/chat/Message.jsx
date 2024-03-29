3334509d6535943a811d319e2e38d47f9c5939ce
Moin3 <moinislam667@gmail.com>
2024-03-29 06:51:12 +0600

    file extension image setup

--- src/components/chat/Message.jsx
+++ src/components/chat/Message.jsx
@@ -4,7 +4,6 @@ import { downloadMedia, formatDate } from '../../utils/common-utils';
 import { MdDownloadForOffline } from "react-icons/md";
 import React, { forwardRef } from 'react';
 
-
 const Wrapper = styled(Box)`
     background: #6dd7f4;
     padding:1px 5px;
@@ -30,14 +29,14 @@ const Own = styled(Box)`
 const Text = styled(Typography)`
     font-size: 14px;
     padding: 0 25px 0 5px;
-    font-family:Quicksand;
-    font-weight:500;
-    color:black
+    font-family: Quicksand;
+    font-weight: 500;
+    color: black;
 `;
 
 const Time = styled(Typography)`
-    font-family:Quicksand;
-    font-weight:semi-bold;
+    font-family: Quicksand;
+    font-weight: semi-bold;
     font-size: 10px;
     color: #373636;
     word-break: keep-all;
@@ -45,13 +44,12 @@ const Time = styled(Typography)`
 `;
 
 const Message = forwardRef(({ message }, ref) => {
-    const {user}=userAuth()
-
+    const { user } = userAuth();
 
     return (
         <div ref={ref}>
         {
-            user?._id === message.senderId ? 
+            user ?._id === message.senderId ? 
                 <Own>
                     {
                         message.type === 'file' ? <ImageMessage message={message} /> : <TextMessage message={message} />
@@ -70,38 +68,82 @@ const Message = forwardRef(({ message }, ref) => {
 });
 
 const TextMessage = ({ message }) => {
-    
     return (
         <>
             <Text>{message.text}</Text>
             <Time>{formatDate(message.createdAt)}</Time>
         </>
-    )
-}
-
-
+    );
+};
 
 const ImageMessage = ({ message }) => {
+    const getFileIcon = (fileType) => {
+        switch (fileType) {
+            case 'pdf':
+                return 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/PDF_file_icon.svg/1667px-PDF_file_icon.svg.png';
+            case 'doc':
+            case 'docx':
+                return 'https://google.oit.ncsu.edu/wp-content/uploads/sites/6/2021/01/Google_Docs.max-2800x2800-1.png';
+            case 'txt':
+                return 'https://cdn-icons-png.flaticon.com/512/29/29076.png';
+            case 'ppt':
+            case 'pptx':
+                return 'https://cdn-icons-png.flaticon.com/512/4726/4726016.png';
+            case 'mp4':
+            case 'mkv':
+            case 'wmv':
+            case 'webm':
+                return 'https://t3.ftcdn.net/jpg/01/09/40/34/360_F_109403483_qocRmeSFXJ6KlF3yoaDBuI3CZOiNGfCw.jpg';
+            case 'zip':
+                return 'https://www.iconpacks.net/icons/2/free-zip-icon-1519-thumb.png';
+            default:
+                return 'https://icons.veryicon.com/png/o/file-type/system-icon/unknown-file-types.png';
+        }
+    };
+
+    const getFileType = (fileName) => {
+        const extension = fileName.split('.').pop().toLowerCase();
+        switch (extension) {
+            case 'pdf':
+            case 'doc':
+            case 'docx':
+            case 'txt':
+            case 'ppt':
+            case 'pptx':
+            case 'mp4':
+            case 'mkv':
+            case 'wmv':
+            case 'webm':
+                return extension;
+            default:
+                return 'unknown';
+        }
+    };
+
+    const renderFileMessage = () => {
+        const fileType = getFileType(message.text);
+        if (fileType !== 'unknown') {
+            return (
+                <Box style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: 10 }}>
+                    <img src={getFileIcon(fileType)} alt={`${fileType}-icon`} style={{ width: 60, padding: 6 }} />
+                    <Typography style={{ fontSize: 12, width: '100%', maxWidth: '350px', fontFamily: 'Quicksand', fontWeight: '500' }}>{message.text.split("/").pop()}</Typography>
+                </Box>
+            );
+        } else {
+            // Handle unknown file types
+            return <Text>{message.text}</Text>;
+        }
+    };
 
     return (
         <Box style={{ position: 'relative' }}>
-            {
-                message?.text?.includes('.pdf') ?
-                    <Box style={{ display: 'flex',justifyContent:'center',alignItems:'center',gap:10}}>
-                        <img src={'https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/PDF_file_icon.svg/1667px-PDF_file_icon.svg.png'} alt="pdf-icon" style={{ width: 60,padding:6 }} />
-                        <Typography style={{ fontSize: 12,width:'100%',maxWidth:'350px',fontFamily:'Quicksand',fontWeight:'500' }} >{message.text.split("/").pop()}</Typography>
-                    </Box>
-                : 
-                    <img style={{ width: "100%",maxWidth:'300px', height: '100%',maxHeight:'250px', objectFit: 'contain' }} src={message.text} alt={message.text} />
-            }
-            
-            <Time style={{ position: 'absolute', bottom: 3, right: 0 ,backgroundColor:'#577799',borderRadius: '2px',color:'white',padding:'0 4px',display:'flex',flexDirection:'row',justifyContent:'center',alignItems:'center',gap:'10px'}}>
-                <MdDownloadForOffline style={{cursor:'pointer'}} onClick={(e)=> downloadMedia(e,message.text)} />
+            {renderFileMessage()}
+            <Time style={{ position: 'absolute', bottom: 3, right: 0, backgroundColor: '#577799', borderRadius: '2px', color: 'white', padding: '0 4px', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', gap: '10px' }}>
+                <MdDownloadForOffline style={{ cursor: 'pointer' }} onClick={(e) => downloadMedia(e, message.text)} />
                 {formatDate(message.createdAt)}
             </Time>
         </Box>
-    )
-}
-
+    );
+};
 
 export default Message;
