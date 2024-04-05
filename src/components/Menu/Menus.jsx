b7e34cfa9e0996e517272edd92540a934e437e31
Moin3 <moinislam667@gmail.com>
2024-04-05 06:03:05 +0600

    deploy

--- src/components/Menu/Menus.jsx
+++ src/components/Menu/Menus.jsx
@@ -1,5 +1,15 @@
-import * as React from 'react';
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { getAPI } from '../../services/api';
+import { toast } from 'react-hot-toast';
+import Conversations from './Conversations';
+import { userInfo } from '../../context/UserProvider';
+import Modal from '../reusable/Modal';
+import ProfileSettings from '../chat/ProfileSettings';
+import SearchMenu from '../search/SearchMenu';
+import { Link } from 'react-router-dom';
+import { TiHome } from 'react-icons/ti';
+import { BiSolidExit } from 'react-icons/bi';
+import { Tooltip, Typography, styled } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
@@ -7,166 +17,171 @@ import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
-import ProfileSettings from '../chat/ProfileSettings';
-import { TiHome } from "react-icons/ti";
-import { BiSolidExit } from "react-icons/bi";
-import {Tooltip, Typography, styled} from '@mui/material/';
-import SearchMenu from '../search/SearchMenu';
-import { Link } from 'react-router-dom';
-import { getAPI } from '../../services/api';
-import {toast} from 'react-hot-toast'
-import Conversations from './Conversations';
-import { userInfo } from '../../context/UserProvider';
-import Modal from '../reusable/Modal';
-
-
-
-
 
 const drawerWidth = 240;
 
-const ChattingHeader=styled(AppBar)`
+const ChattingHeader = styled(AppBar)`
     background: #577799;
     box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
     backdrop-filter: blur(5px);
     -webkit-backdrop-filter: blur(5px);
-    border-radius:10px;
-`
-
+    border-radius: 10px;
+`;
 
 const Menus = () => {
-    const [users, setUsers] = React.useState([]);
+    const [users, setUsers] = useState([]);
     const { setPerson } = userInfo();
-    const [searchText,setSearchText]=React.useState('')
-    const [mobileOpen, setMobileOpen] = React.useState(false);
+    const [searchText, setSearchText] = useState('');
+    const [mobileOpen, setMobileOpen] = useState(false);
     const [isModalOpen, setIsModalOpen] = useState(false);
 
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
+    };
 
-   const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
+    const handleOpenModal = () => {
+        setIsModalOpen(true);
+    };
 
-  const handleOpenModal = () => {
-    setIsModalOpen(true);
-  };
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    };
 
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
-
-  const drawer = (
-    <Box>
-      <Toolbar>
-        <Box sx={{display:'flex',flexDirection:'row',width:'100%',justifyContent:'space-around',alignItems:'space-between'}}>
-            <Link onClick={()=>setPerson(null)} to={'/intro'}>
-            <Tooltip title="Intro Page">
-              <Typography sx={{fontSize:'30px',color:'black',
-                    '&:hover': {
-                      color: 'gray',
-                    }}}> 
-                <TiHome/>
-              </Typography>
-            </Tooltip>
-            </Link>
-            <Tooltip title="Logout">
-              <Typography onClick={handleOpenModal} sx={{fontSize:'30px',color:'black',cursor:'pointer',
-                      '&:hover': {
-                        color: 'gray',
-                      }}}>
-                  <BiSolidExit />
-              </Typography>
-            </Tooltip> 
+    const drawer = (
+        <Box>
+            <Toolbar>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        width: '100%',
+                        justifyContent: 'space-around',
+                        alignItems: 'space-between',
+                    }}
+                >
+                    <Link onClick={() => setPerson(null)} to="/intro">
+                        <Tooltip title="Intro Page">
+                            <Typography
+                                sx={{
+                                    fontSize: '30px',
+                                    color: 'black',
+                                    '&:hover': {
+                                        color: 'gray',
+                                    },
+                                }}
+                            >
+                                <TiHome />
+                            </Typography>
+                        </Tooltip>
+                    </Link>
+                    <Tooltip title="Logout">
+                        <Typography
+                            onClick={handleOpenModal}
+                            sx={{
+                                fontSize: '30px',
+                                color: 'black',
+                                cursor: 'pointer',
+                                '&:hover': {
+                                    color: 'gray',
+                                },
+                            }}
+                        >
+                            <BiSolidExit />
+                        </Typography>
+                    </Tooltip>
+                </Box>
+            </Toolbar>
+            <Toolbar
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    fontWeight: 500,
+                    fontFamily: 'Acme',
+                    textTransform: 'uppercase',
+                }}
+            >
+                <SearchMenu setSearchText={setSearchText} />
+            </Toolbar>
+            <Divider />
+            <Conversations users={users} />
+            <Modal isOpen={isModalOpen} onClose={handleCloseModal} />
         </Box>
-      </Toolbar>
-      <Toolbar sx={{display:'flex',justifyContent:'center',alignItems:'center',fontWeight:500,fontFamily:'Acme',textTransform:'uppercase'}}>
-        <SearchMenu setSearchText={setSearchText}/>  {/*search functionality*/}
-      </Toolbar>
-      <Divider />
-      {/* conversation functionality */}
-      <Conversations users={users}/> 
-      <Modal isOpen={isModalOpen} onClose={handleCloseModal} />         
-    </Box>
-  );
+    );
 
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                let allUsers = await getAPI('/all_users');
+                let usersData = allUsers.data;
+                let filteredData = usersData.filter((userDetail) =>
+                    `${userDetail?.first_name} ${userDetail?.last_name}`.toLowerCase().includes(searchText.toLocaleLowerCase())
+                );
+                setUsers(filteredData);
+            } catch (err) {
+                toast.error(err.message);
+            }
+        };
+        fetchData();
+    }, [searchText]);
 
-    
-
-  React.useEffect(() => {
-    const fetchData = async () => {
-     try{
-      let allUsers = await getAPI('/all_users')
-      let usersData=allUsers.data
-      let fiteredData = usersData.filter(userDetail => 
-        `${userDetail?.first_name} ${userDetail?.last_name}`.toLowerCase().includes(searchText.toLocaleLowerCase())
-      );      
-      setUsers(fiteredData)
-     }catch(err){
-      toast.error(err.message)
-     }
-      
-  }
-  fetchData();
-  }, [searchText]);
-
-    
-  return (  
-    <Box>
-      <ChattingHeader
-        position="fixed"
-        sx={{
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-          ml: { sm: `${drawerWidth}px` },
-          boxShadow:'none'
-        }}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
-          >
-            <MenuIcon />
-          </IconButton>
-          {/* /* --------------------- ProfileSettings is impoted here -------------------- */}
-          <Box sx={{width:'100%',display:'flex',justifyContent:'flex-end',mr:2}}>
-            <ProfileSettings/>
-          </Box>
-        </Toolbar>
-      </ChattingHeader>
-        <Box
-          component="nav"
-          sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-          aria-label="mailbox folders"
-        >
-          <Drawer
-            variant="temporary"
-            open={mobileOpen}
-            onClose={handleDrawerToggle}
-            ModalProps={{
-              keepMounted: true, // Better open performance on mobile.
-            }}
-            sx={{
-              display: { xs: 'block', sm: 'none' },
-              '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-            }}
-          >
-            {drawer}
-          </Drawer>
-          <Drawer
-            variant="permanent"
-            sx={{
-              display: { xs: 'none', sm: 'block' },
-              '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-            }}
-            open
-          >
-            {drawer}
-          </Drawer>
+    return (
+        <Box>
+            <ChattingHeader
+                position="fixed"
+                sx={{
+                    width: { sm: `calc(100% - ${drawerWidth}px)` },
+                    ml: { sm: `${drawerWidth}px` },
+                    boxShadow: 'none',
+                }}
+            >
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                        onClick={handleDrawerToggle}
+                        sx={{ mr: 2, display: { sm: 'none' } }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Box sx={{ width: '100%', display: 'flex', justifyContent: 'flex-end', mr: 2 }}>
+                        <ProfileSettings />
+                    </Box>
+                </Toolbar>
+            </ChattingHeader>
+            <Box
+                component="nav"
+                sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+                aria-label="mailbox folders"
+            >
+                <Drawer
+                    variant="temporary"
+                    open={mobileOpen}
+                    onClose={handleDrawerToggle}
+                    ModalProps={{
+                        keepMounted: true, // Better open performance on mobile.
+                    }}
+                    sx={{
+                        display: { xs: 'block', sm: 'none' },
+                        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                    }}
+                >
+                    {drawer}
+                </Drawer>
+                <Drawer
+                    variant="permanent"
+                    sx={{
+                        display: { xs: 'none', sm: 'block' },
+                        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                    }}
+                    open
+                >
+                    {drawer}
+                </Drawer>
+            </Box>
         </Box>
-    </Box>
-  )
-}
+    );
+};
 
-export default Menus
+export default Menus;
--- src/pages/IntroPage.jsx
+++ src/pages/IntroPage.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import EmptyChatBox from '../components/chat/EmptyChatBox';
@@ -6,25 +6,23 @@ import Menus from '../components/menu/Menus';
 import { userAuth } from '../context/AccountProvider';
 import { useNavigate } from 'react-router-dom';
 
-
 function IntroPage(props) {
-    const navigate=useNavigate()
-    const {user,token}=userAuth()
+    const navigate = useNavigate();
+    const { user, token } = userAuth();
 
-    React.useEffect(()=>{
-        if(!user && !token){
-          navigate('/signin')
+    React.useEffect(() => {
+        if (!user && !token) {
+            navigate('/signin');
         }
-      },[token,user])
+    }, [token, user]);
 
-  return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-        <Menus/>
-        <EmptyChatBox/>
-    </Box>
-  );
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <Menus />
+            <EmptyChatBox />
+        </Box>
+    );
 }
-
 
 export default IntroPage;
