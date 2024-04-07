88a603c9c01874ae66cdc0dc3add0cb80ae5f772
Moin3 <moinislam667@gmail.com>
2024-04-07 14:05:29 +0600

    google-clint id setup

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,8 @@ import SocketProvider from './context/SocketProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+    {/* <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}> */}
+    <GoogleOAuthProvider clientId='356324940055-2fipp94p0ud6a5d34jk4fatd9117v3vj.apps.googleusercontent.com'>
       <MsgProvider>
         <AccountProvider>
           <UserProvider>
